019a0370fe1ffe7399ff58dea63aef82ab0e9230
GBrunheri <77868231+GBrunheri@users.noreply.github.com>
2024-10-09 14:48:54 -0300

    Rankeador de Heróis from hell
    *referencia ao primeiro commit do github*

---
+++ index.js
@@ -0,0 +1,52 @@
+
+
+let nome = "Misterioso"
+let saldo = saldoVitorias(45, 13)
+console.log("O Herói " + nome + " tem um saldo de " + saldo + " vitorias e está no nível " + nivelRanking(saldo))
+
+
+
+// Funções
+
+function saldoVitorias(vitorias, derrotas) {
+    let saldo = vitorias - derrotas
+    return saldo
+
+}
+
+function nivelRanking(saldo) {
+
+    switch (true) {
+        case saldo <= 10:
+            nivel =  "Ferro"
+            break
+
+        case saldo <= 20:
+            nivel = "Bronze"
+            break
+
+        case saldo <= 50:
+            nivel = "Prata"
+            break
+
+        case saldo <= 80:
+            nivel = "Ouro"
+            break
+
+        case saldo <= 90:
+           nivel = "Diamante"
+            break
+
+        case saldo <= 100:
+            nivel = "Lendário"
+            break
+
+        case saldo >= 100:
+            nivel = "Imortal"
+            break
+    }
+
+return nivel
+}
+
+    
